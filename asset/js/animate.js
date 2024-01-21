41db79f7598372fb6c275899c837dd08c8f96345
Dikry720p <dikrynurahman49@gmail.com>
2024-01-21 23:49:17 +0700

    upload 1
    versi awal atau sebuah percobaan

---
+++ asset/js/animate.js
@@ -0,0 +1,83 @@
+// My Methods
+var myObj = {
+    // Select TextArea Func
+    textSelect: function () {
+        document.getElementById('description').select();
+    },
+
+    // Hide form Method
+    hide: function () {
+        document.getElementById("form").style.display = "none";
+        document.getElementById("show").style.display = "inline-block";
+    },
+
+    // Show Form Method
+    show: function () {
+        document.getElementById("form").style.display = "block";
+        document.getElementById("show").style.display = "none";
+        document.getElementById('myDate').valueAsDate = new Date();
+    },
+
+    // Removing task method
+    removeTask: function () {
+        var id = Array.prototype.indexOf.call(this.parentElement.children, this);
+        var myTasks = returnToDo();
+        myTasks.splice(id, 1);
+        localStorage.setItem('myData', JSON.stringify(myTasks));
+        document.getElementById('myTasks').innerHTML = '';
+        showMyTasks();
+    }
+};
+
+// Checks if there is already data in LocalStorage
+function returnToDo() {
+    var myTasks = [];
+    var myTasksTemp = localStorage.getItem('myData');
+    if (myTasksTemp != null) {
+        myTasks = JSON.parse(myTasksTemp);
+    }
+    return myTasks;
+}
+
+// Class that creates tasks.
+function Task() {
+    this.name = document.getElementById('name').value;
+    this.subject = document.getElementById('subject').value;
+    this.date = document.getElementById('myDate').value;
+    this.describe = document.getElementById('description').value;
+}
+
+// Insert task properties into the HTML
+function newTask(x, y, z, o) {
+    document.getElementById('myTasks').innerHTML +=
+        '<div class="col l3 m4 s12 animated zoomIn"> <h4>' + z + ':</h4>' +
+        '<p>' + y + '</p>' +
+        '<p>By: ' + x + '</p>' +
+        '<p>Due: ' + o + '</p>' +
+        '<div class="btn red" onclick="myObj.removeTask.call(this)">Delete</div>' +
+        '</div>';
+}
+
+// Gets all the objects from the array.
+function showMyTasks() {
+    var myTasks = returnToDo();
+    document.getElementById('myTasks').innerHTML = '';
+    for (var i = 0; i < myTasks.length; i++) {
+        newTask(
+            myTasks[i].name,
+            myTasks[i].describe,
+            myTasks[i].subject,
+            myTasks[i].date
+        );
+    }
+}
+
+function submitInfo() {
+    var myTasks = returnToDo();
+    myTasks.push(new Task());
+    localStorage.setItem('myData', JSON.stringify(myTasks));
+    showMyTasks();
+    myObj.hide();
+}
+
+showMyTasks();
