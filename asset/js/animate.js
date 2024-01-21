529c98b9b3345d1578e969db2828272c4d3475c9
Dikry720p <dikrynurahman49@gmail.com>
2024-01-22 00:09:11 +0700

    upload 2
    perubahan kalimat di html

--- asset/js/animate.js
+++ asset/js/animate.js
@@ -1,68 +1,66 @@
-// My Methods
-var myObj = {
-    // Select TextArea Func
-    textSelect: function () {
+//My Methods
+var myObj= {
+    //Select TextArea Func
+    textSelect: function(){
         document.getElementById('description').select();
     },
 
-    // Hide form Method
-    hide: function () {
-        document.getElementById("form").style.display = "none";
-        document.getElementById("show").style.display = "inline-block";
-    },
+//Hide form Method
+    hide: function() {
+    document.getElementById("form").style.display = "none";
+    document.getElementById("show").style.display = "inline-block";
 
-    // Show Form Method
-    show: function () {
-        document.getElementById("form").style.display = "block";
-        document.getElementById("show").style.display = "none";
-        document.getElementById('myDate').valueAsDate = new Date();
-    },
+},
+//Show Form Method
+    show:function(){
 
-    // Removing task method
+    document.getElementById("form").style.display = "block";
+    document.getElementById("show").style.display = "none";
+    document.getElementById('myDate').valueAsDate = new Date();
+    },
+    //Removing task method
     removeTask: function () {
-        var id = Array.prototype.indexOf.call(this.parentElement.children, this);
-        var myTasks = returnToDo();
-        myTasks.splice(id, 1);
-        localStorage.setItem('myData', JSON.stringify(myTasks));
-        document.getElementById('myTasks').innerHTML = '';
-        showMyTasks();
-    }
-};
+    var id = this.getAttribute('id');
+    var myTasks = returnToDo();
+    myTasks.splice(id, 1);
+    localStorage.setItem('myData', JSON.stringify(myTasks));
+    document.getElementById('myTasks').innerHTML = '';
+    showMyTasks();
+    console.log('delete');
 
-// Checks if there is already data in LocalStorage
-function returnToDo() {
+}
+};
+//Checks if there is already data in LocalStorage
+function returnToDo(){
     var myTasks = [];
     var myTasksTemp = localStorage.getItem('myData');
-    if (myTasksTemp != null) {
+    if(myTasksTemp != null){
         myTasks = JSON.parse(myTasksTemp);
     }
     return myTasks;
 }
-
-// Class that creates tasks.
-function Task() {
+//Class that creates tasks.
+function Task(){
     this.name = document.getElementById('name').value;
     this.subject = document.getElementById('subject').value;
     this.date = document.getElementById('myDate').value;
     this.describe = document.getElementById('description').value;
 }
-
-// Insert task properties into the HTML
-function newTask(x, y, z, o) {
+//Insert task properties into the HTML
+function newTask(x,y,z,o){
     document.getElementById('myTasks').innerHTML +=
-        '<div class="col l3 m4 s12 animated zoomIn"> <h4>' + z + ':</h4>' +
-        '<p>' + y + '</p>' +
-        '<p>By: ' + x + '</p>' +
-        '<p>Due: ' + o + '</p>' +
-        '<div class="btn red" onclick="myObj.removeTask.call(this)">Delete</div>' +
-        '</div>';
+        '<div class="col l3 m4 s12 animated zoomIn"> <h4>'+z+  ':</h1>'+
+        '<p>'+y+'</p>' +
+        '<p>By: '+x+'</p>' +
+        '<p>Due: ' +o +'</p>'+
+        '<div class="btn red" >Delete</div>'+
+    '</div>'
 }
-
-// Gets all the objects from the array.
-function showMyTasks() {
+//Gets all the objects from the array.
+function showMyTasks(){
     var myTasks = returnToDo();
     document.getElementById('myTasks').innerHTML = '';
-    for (var i = 0; i < myTasks.length; i++) {
+    for(var i=0;i<myTasks.length;i++){
         newTask(
             myTasks[i].name,
             myTasks[i].describe,
@@ -70,14 +68,18 @@ function showMyTasks() {
             myTasks[i].date
         );
     }
-}
+    var button = document.getElementsByClassName('red');
+    for (var j = 0; j < button.length; j++) {
+        button[j].addEventListener('click', myObj.removeTask);
+        console.log(button[j].addEventListener('click', myObj.removeTask));
 
-function submitInfo() {
+    }
+}
+function submitInfo(){
     var myTasks = returnToDo();
-    myTasks.push(new Task());
-    localStorage.setItem('myData', JSON.stringify(myTasks));
+    myTasks.push(new Task);
+    localStorage.setItem('myData',JSON.stringify(myTasks));
     showMyTasks();
     myObj.hide();
 }
-
 showMyTasks();
